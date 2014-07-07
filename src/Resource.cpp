0f59f634540e9f475550cfacf21656932639927e
Boris Nagaev <bnagaev@gmail.com>
2014-07-07 18:59:00 +0400

    Resource: show empty GIF if image has not been rendered

--- src/Resource.cpp
+++ src/Resource.cpp
@@ -12,10 +12,23 @@ Resource::~Resource() {
     beingDeleted();
 }
 
+static const char EMPTY_GIF[] =
+  { 0x47, 0x49, 0x46, 0x38, 0x39, 0x61, 0x01, 0x00, 0x01, 0x00,
+    0x80, 0x00, 0x00, 0xdb, 0xdf, 0xef, 0x00, 0x00, 0x00, 0x21,
+    0xf9, 0x04, 0x01, 0x00, 0x00, 0x00, 0x00, 0x2c, 0x00, 0x00,
+    0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x02, 0x02, 0x44,
+    0x01, 0x00, 0x3b };
+const int EMPTY_GIF_SIZE = 43;
+
 void Resource::handleRequest(const Http::Request& request,
                              Http::Response& response) {
-    response.setMimeType("image/png");
     QByteArray ba = qiwApp->bridge()->image();
-    response.out().write(ba.constData(), ba.size());
+    if (ba.isEmpty()) {
+        response.setMimeType("image/gif");
+        response.out().write(EMPTY_GIF, EMPTY_GIF_SIZE);
+    } else {
+        response.setMimeType("image/png");
+        response.out().write(ba.constData(), ba.size());
+    }
 }
 
