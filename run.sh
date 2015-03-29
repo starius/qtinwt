8b7053416560b4141c7547131a16a9fb7f29709f
Boris Nagaev <bnagaev@gmail.com>
2015-03-29 17:05:40 +0300

    run.sh: start Xvfb manually, enable X Render
    X render is needed for antialiasing.

--- run.sh
+++ run.sh
@@ -2,4 +2,11 @@
 # Copyright (C) 2014-2015 Boris Nagaev
 # See the LICENSE file for terms of use
 
-xvfb-run src/qtinwt --docroot /usr/share/Wt/ --http-address 0.0.0.0 --http-port 30522
+Xvfb :99 -screen 0 1024x768x24 +extension RANDR +render > /dev/null 2>&1 &
+XVFB_PID=$!
+export DISPLAY=:99
+
+./src/qtinwt --docroot /usr/share/Wt/ \
+    --http-address 0.0.0.0 --http-port 30522
+
+kill $XVFB_PID
